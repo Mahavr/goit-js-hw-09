b75b35f81076f9a6fa4e44998778778a0d9632d7
Mahavr <95856803+Mahavr@users.noreply.github.com>
2023-12-12 20:09:22 +0000

    Deploying to gh-pages from @ Mahavr/goit-js-hw-09@1d9086309fd24ca83947de015208f2dc6b9b02f9 🚀

---
+++ 01-color-switcher.0b3ca787.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");t.addEventListener("click",(function(n){n.preventDefault(),a=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0)),t.disabled=!0,e.disabled=!1}),1e3)})),e.addEventListener("click",(function(n){clearInterval(a),t.disabled=!1,e.disabled=!0}));var a=null;e.disabled=!0}();
+//# sourceMappingURL=01-color-switcher.0b3ca787.js.map
---
+++ 01-color-switcher.2a70e3ce.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");t.addEventListener("click",(function(a){a.preventDefault(),d=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`,t.disabled=!0,e.disabled=!1}),1e3)})),e.addEventListener("click",(function(a){clearInterval(d),t.disabled=!1,e.disabled=!0}));let d=null;e.disabled=!0;
+//# sourceMappingURL=01-color-switcher.2a70e3ce.js.map
